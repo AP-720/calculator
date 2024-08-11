9740012eae6c4236f3f22c6d3e31c42de934e0db
Andy Price <andyprice720@gmail.com>
2024-08-11 16:05:33 +0100

    Add More Functionality
    Added AllClear button function. Tweaked so that calculator would not evaluate
    more than a single pair of numbers at a time.

--- script.js
+++ script.js
@@ -1,79 +1,135 @@
 
-const firstNum = '';
-const secondNum = '';
-const operator = '';
-let displayValue = '';
-
 const display = document.querySelector('.screen');
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
-const equalsButton = document.querySelectorAll('.equals');
+const equalsButton = document.querySelector('.equals');
+const allClear = document.querySelector('.all-clear')
 
+let displayValue = '';
 
 const calcMemory = {
     firstNum: '',
     operator: '',
     secondNum: '',
+    perviousSum: '',
 };
+
 
 // Number buttons 
 numberButtons.forEach((button) => {
     button.addEventListener("click", () => {
 
-        if (calcMemory.secondNum === '' && calcMemory.operator === '') {    
+        if (calcMemory.operator === '' && calcMemory.secondNum === '') {    
         displayValue += button.textContent;
         display.textContent = displayValue;
-        calcMemory.firstNum = displayValue;
-        }
+        calcMemory.firstNum = parseInt(displayValue);
+    
+        console.log(calcMemory.firstNum)
         
-        if (calcMemory.operator !== ''){
+        } else if (calcMemory.firstNum !== '' && calcMemory.operator !== '' &&calcMemory.perviousSum !== '') {
         displayValue += button.textContent;
         display.textContent = displayValue;
-        calcMemory.secondNum = displayValue;
-        }
+        calcMemory.secondNum = parseInt(display.textContent);
+        console.log(calcMemory.secondNum)
+        console.log(displayValue)
+
+        } else if (calcMemory.firstNum !== '' && calcMemory.operator !== '') {
+        displayValue += button.textContent;
+        display.textContent = displayValue;
+        calcMemory.secondNum = parseInt(displayValue);
+        console.log(calcMemory.secondNum);
+        } 
+        console.table(calcMemory)
     })
 });
+
 
 // Operator buttons 
 operatorButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        // displayValue = "";
+        
+        if (calcMemory.secondNum === '') {
+        calcMemory.operator === ''
         displayValue += ' ' + button.textContent;
         display.textContent = displayValue;
         calcMemory.operator = button.textContent;
         displayValue = '';
-    })
-});
+        console.log(displayValue)
+        console.log(calcMemory.operator)
+        } else {
+        operate(calcMemory.firstNum, calcMemory.operator, calcMemory.secondNum);
+        calcMemory.perviousSum = result;
+        display.textContent = result + ' ' + button.textContent
+        displayValue = '';
+        calcMemory.firstNum = calcMemory.perviousSum;
+        calcMemory.secondNum = '';
+        // calcMemory.perviousSum = '';
+        calcMemory.operator = button.textContent;
+        console.table(calcMemory)
+        console.log(displayValue)
+        }
+    
+    }
+    
+)});
+
+
+ 
 
 // Equals button
-equalsButton.forEach((button) => {
-    button.addEventListener("click", () => {
-        let numOne = calcMemory.firstNum;
-        let operatorSign = calcMemory.operator;
-        let numTwo = calcMemory.secondNum;
-        display.textContent = operate(numOne, operatorSign, numTwo);
-    })
-});
+equalsButton.addEventListener('click', () => {
+    
+    if (calcMemory.secondNum === '' || calcMemory.operator === '') {
+        resetCalMemory();
+    } else {
+        operate(calcMemory.firstNum, calcMemory.operator, calcMemory.secondNum);
+        calcMemory.perviousSum = result;
+        display.textContent = result;
+        displayValue = result;
+        calcMemory.firstNum = calcMemory.perviousSum;
+        calcMemory.secondNum = '';
+        calcMemory.perviousSum = '';
+        calcMemory.operator = '';
+        }
+        console.table(calcMemory)
+    });
 
 
+
+// All Clear Button
+allClear.addEventListener('click', () => {
+                resetCalMemory();
+                return;
+    });
+
+function resetCalMemory() {
+    calcMemory.firstNum = '';
+    calcMemory.operator = '';
+    calcMemory.secondNum = '';
+    calcMemory.perviousSum = '';
+    display.textContent = '';
+    displayValue = '';
+};
+
+// calculate function 
 function operate(numOne, operatorSign, numTwo) {
     if (operatorSign === '+') {
-        return add(numOne, numTwo);
+        result = add(numOne, numTwo);
     }
     if (operatorSign === '-') {
-        return subtract(numOne, numTwo);
+        result = subtract(numOne, numTwo);
     }
     if (operatorSign === 'x') {
-        return multiply(numOne, numTwo);
+        result = multiply(numOne, numTwo);
     }
     if (operatorSign === '÷') {
-        return divide(numOne, numTwo);
+        result = divide(numOne, numTwo);
     }
 }
 
 // Basic calculator functions
 function add(a, b) {
-    return parseInt(a) + parseInt(b);
+    return a + b;
 };
 
 function subtract(a, b) {
@@ -87,4 +143,3 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
-
